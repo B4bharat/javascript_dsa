c667813ca28983427ba1860771f265d879444428
Bharat Poptwani <bharat.poptwani@bookmyshow.com>
2019-11-11 19:33:40 +0530

    initial commit

---
+++ Javascript/queue/fundamentals.js
@@ -0,0 +1,34 @@
+function Queue() {
+    this.dataStore = [];
+    this.enqueue = enqueue;
+    this.dequeue = dequeue;
+    this.front = front;
+    this.back = back;
+    this.toString = toString;
+    this.empty = empty;
+}
+
+function enqueue(element) {
+    this.dataStore.push(element);
+}
+
+function dequeue() {
+    return this.dataStore.shift();
+}
+
+function front() {
+    return this.dataStore[0];
+}
+
+function back() {
+    return this.dataStore[this.dataStore.length - 1];
+}
+
+function empty() {
+    if (this.dataStore.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
---
+++ Javascript/queue/radix-sort.js
@@ -0,0 +1,14 @@
+/**
+ * STUDY MATERIALS:
+ * - https://www.geeksforgeeks.org/counting-sort/
+ * - https://www.youtube.com/watch?v=cOBcekmPWaM
+ * - https://www.interviewcake.com/concept/java/counting-sort
+ * - https://medium.com/basecs/counting-linearly-with-counting-sort-cd8516ae09b3
+ * - https://www.cs.usfca.edu/~galles/visualization/CountingSort.html
+ * - https://www.youtube.com/watch?v=OKd534EWcdk
+ * - https://www.youtube.com/watch?v=XiuSW_mEn7g
+ */
+
+/**
+ * DESCRIPTION:
+ */
